10052a9872ae8f8808d1608fdaffe98f7edc923a
adrianstopa15 <adrians@student.kul.pl>
2024-03-20 18:46:52 +0100

    ciasteczka test

--- src/main/java/pl/katering/katering/controllers/AuthenticationController.java
+++ src/main/java/pl/katering/katering/controllers/AuthenticationController.java
@@ -1,14 +1,18 @@
 package pl.katering.katering.controllers;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.katering.katering.classes.AuthenticationResponse;
 import pl.katering.katering.classes.User;
 import pl.katering.katering.services.AuthenticationService;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class AuthenticationController {
 
@@ -25,7 +29,18 @@ public class AuthenticationController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> login(@RequestBody User request) {
-        return ResponseEntity.ok(authService.authenticate(request));
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody User request, HttpServletResponse response) {
+        AuthenticationResponse authResponse = authService.authenticate(request);
+        if (authResponse != null && authResponse.getToken() != null) {
+            Cookie cookie = new Cookie("authToken", authResponse.getToken());
+            cookie.setHttpOnly(true);
+            cookie.setSecure(false);
+            cookie.setPath("/");
+          cookie.setMaxAge(24 * 60 * 60);
+            response.addCookie(cookie);
+            return ResponseEntity.ok(authResponse);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
     }
 }
