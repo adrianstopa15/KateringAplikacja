45961ee92f822ff6b83403271ab912f589886350
dzienbardzodobry <arrow.ronaldo7@gmail.com>
2024-04-04 23:18:35 +0200

    sila hasla

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import "./App.css";
 import MainPage from "./mainPage";
 import "./mainPageStyle.css";
 import "./profilePages.css";
-<<<<<<< HEAD
-import Alert from "react-bootstrap/Alert";
-import ProfilePage from "./profilePages";
-import SimplifiedProfilePage from "./simplifiedProfilePage";
+
 
 function App() {
-  return <ProfilePage />;
+  return <MainPage />;
 }
 
 export default App;
