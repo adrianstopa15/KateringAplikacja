4971a0bcceeacf5c2787a6eec7205ce50c462636
adrianstopa15 <adrians@student.kul.pl>
2024-04-04 23:27:52 +0200

    poprawki

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import MainPage from "./mainPage";
 import "./mainPageStyle.css";
 import "./profilePages.css";
-<<<<<<< HEAD
 import Alert from "react-bootstrap/Alert";
 import ProfilePage from "./profilePages";
 import SimplifiedProfilePage from "./simplifiedProfilePage";
